528694a15f27ee52d120e001ebbaba7276c7c5e2
harshdeepkaur52510 <harshdeepkaur@my.jcu.edu.au>
2023-06-13 14:11:06 +1000

    Add broken score program

---
+++ prac01/broken_score.py
@@ -0,0 +1,13 @@
+"""
+Broken program to determine score status
+"""
+
+score = float(input("Enter the score: "))
+if score < 0 or score > 100:
+    print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
